31dabba0388f2bdda680c37d94102a44889703c5
Olayinka Bidemi Ayeni <65237847+YinkTech@users.noreply.github.com>
2022-06-30 17:48:42 +0100

    add, delete, chechlist uncompleted, clear al

---
+++ src/addtask.js
@@ -0,0 +1,46 @@
+import { inputBut } from './form.js';
+inputBut();
+
+export const add = (taskArray) => {
+    const taskTitle = document.getElementById('title');
+    const taskDate = document.getElementById('date');
+
+    if (taskTitle.value  === '') {
+        return;
+    }
+    const task = {
+        title: taskTitle.value,
+        Date: taskDate.value,
+        completed: false,
+        index: taskArray.length + 1,
+    };
+    
+    taskArray.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+    taskTitle.value = '';
+    taskDate.value = '';
+}
+
+export const removeTask = (taskArray, remTask) => {
+    taskArray = taskArray.filter((task) => task.index !== remTask.index);
+    taskArray.forEach((task) => {
+      task.index = taskArray.indexOf(task) + 1;
+    });
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+  }
+
+export const check = (checkbox, tasklist) => {
+    if (checkbox.checked) {
+      tasklist.completed = true;
+    } else {
+      tasklist.completed = false;
+    }
+}
+
+export const clearChecked = (taskArray) => {
+    taskArray = taskArray.filter((task) => !task.completed);
+    taskArray.forEach((task) => {
+      task.index = taskArray.indexOf(task) + 1;
+    });
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+  }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,70 @@
-import { pageLoad } from "./page-load.js";
+import { add, removeTask, check, clearChecked } from './addtask.js';
+
 import './styles/style.scss';
 
-pageLoad();
+const showList = document.getElementById('showItem');
+
+let myTasks = [];
+
+const saveToStorage = (taskArray) => {
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+}
+
+function show() {
+    showList.innerHTML = "";
+    myTasks.forEach((myTask) => {
+        const item = `<td><input type="checkbox" > </td><td> ${myTask.title} </td><td class="date-rem"> ${myTask.Date} </td><td><button class="delete" id="del" type="button">delete</button></td>`;
+                     const myItem = document.createElement('tr');
+                     myItem.innerHTML = `${item}`;
+                     myItem.classList.add ('myItem');
+                     showList.appendChild(myItem);
+
+                     const checkbox = myItem.firstChild;
+                     checkbox.checked = myTask.completed;
+                     checkbox.addEventListener('change', () => {
+                       check(checkbox, myTask);
+                       saveToStorage(myTasks);
+                     });
+
+                     const remove = myItem.lastChild;
+                        remove.addEventListener('click', () => {
+                            removeTask(myTasks, myTask);
+                            show();
+                            window.location.reload();
+                        });
+    });
+
+    
+}
+
+const enterBtn = document.getElementById('submit');
+enterBtn.onclick = () => {
+    add(myTasks);
+      show();
+};
+
+enterBtn.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      add(myTasks);
+      show();
+    }
+});
+
+const removeAll = document.getElementById('all-item');
+removeAll.addEventListener('click', () => {
+    clearChecked(myTasks);
+    window.location.reload();
+  });
+
+function getFromStorage() {
+    const local = JSON.parse(localStorage.getItem('tasks'));
+    if (local) {
+      myTasks = local;
+    }
+    if (myTasks.length === 0) {
+      showList.innerHTML = '<tr class="list-item"><td class="list-input">To-Do List is empty.</td></tr>';
+    } else {
+      show();
+    }
+  }
+  window.onload = getFromStorage();
